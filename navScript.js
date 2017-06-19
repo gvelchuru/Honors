dc3cc0a898406670a02fe11dc60abfa88bfd466e
gauthv <gvelchuru@gmail.com>
2017-06-18 17:22:05 -0700

    Style changes

---
+++ navScript.js
@@ -0,0 +1,23 @@
+document.getElementById("navMenu").innerHTML =
+    '<div class="container">' +
+    '<div class="navbar-header"> ' +
+    '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> ' +
+    '<span class="icon-bar"></span> ' +
+    '<span class="icon-bar"></span> ' +
+    '<span class="icon-bar"></span> ' +
+    '</button> ' +
+    '<a class="navbar-brand" href="index.html">Gautham Velchuru</a> ' +
+    '</div> ' +
+    '<div class="collapse navbar-collapse" id="myNavbar"> ' +
+    '<ul class="nav navbar-nav navbar-right"> ' +
+    '<li class="dropdown"> ' +
+    '<a class="dropdown-toggle" data-toggle="dropdown" href="projects.html">Projects ' +
+    '<span class="caret"></span></a> ' +
+    '<ul class="dropdown-menu"> ' +
+    '<li><a href="face_mapper.html">FaceMapper</a></li> ' +
+    '</ul> ' +
+    '</li> ' +
+    '<li><a href="http://www.gauthamvelchuru.weebly.com">UW Honors Portfolio</a></li> ' +
+    '</ul> ' +
+    '</div> ' +
+    '</div>';
